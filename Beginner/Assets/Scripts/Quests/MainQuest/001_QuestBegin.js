1e973159a91b48bc369efbe4701e2d2d3ed039d0
shreesha-kedilaya <shreeshakedilaya@gmail.com>
2017-10-01 23:09:26 +0530

    Commit after 21 episodes

---
+++ Beginner/Assets/Scripts/Quests/MainQuest/001_QuestBegin.js
@@ -0,0 +1,8 @@
+﻿var QuestUpdate: GameObject;
+var PlayerText: GameObject;
+var TextDisplay: GameObject;
+
+function Start() {
+	transform.position = Vector3(0,-1000,0);
+	QuestUpdate.GetComponent.<Text>().text = "Exit the wood";
+}
